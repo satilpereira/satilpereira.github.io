f0c4a3ea2a4b15f270b28d81de847f28faec916d
satil_pereira <satil.prog@gmail.com>
2022-04-13 12:37:01 -0300

    Modified styles, scripts and html

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function textTyper() {
+    var i = 0;                  //  set your counter to 0
+    var text = "I design and code websites from scratch";
+    function myLoop() {         //  create a loop function
+        setTimeout(() => {   //  call a 200ms setTimeout when the loop is called
+            document.getElementById("demo").innerHTML += text[i];
+            i++;
+            if (i < text.length) { // Iterates over the string
+                myLoop();
+            }
+        }, 82)
+    }
+    myLoop();
+}
